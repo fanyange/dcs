d16afee92d543e8c67866d1e03896e332bb06a5d
Tom Mao <mr.fanyange@gmail.com>
2016-11-20 22:25:11 +0800

    refactor

--- app/controllers/instructions_controller.rb
+++ app/controllers/instructions_controller.rb
@@ -1,28 +1,20 @@
 class InstructionsController < ApplicationController
-  before_action :set_instruction, only: [:show, :edit, :update, :destroy]
+  before_action :set_document_and_instruction, only: [:show, :edit, :update, :destroy]
 
-  # GET /instructions
-  # GET /instructions.json
   def index
     @instructions = Instruction.all
   end
 
-  # GET /instructions/1
-  # GET /instructions/1.json
   def show
   end
 
-  # GET /instructions/new
   def new
     @instruction = Instruction.new
   end
 
-  # GET /instructions/1/edit
   def edit
   end
 
-  # POST /instructions
-  # POST /instructions.json
   def create
     @document = Document.find(params[:document_id])
     @instruction = @document.instructions.build(instruction_params)
@@ -34,38 +26,28 @@ class InstructionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /instructions/1
-  # PATCH/PUT /instructions/1.json
   def update
-    respond_to do |format|
-      if @instruction.update(instruction_params)
-        format.html { redirect_to @instruction, notice: 'Instruction was successfully updated.' }
-        format.json { render :show, status: :ok, location: @instruction }
-      else
-        format.html { render :edit }
-        format.json { render json: @instruction.errors, status: :unprocessable_entity }
-      end
+    if @instruction.update(instruction_params)
+      redirect_to @document, notice: 'Instruction was successfully updated.' 
+    else
+      render :edit
     end
   end
 
-  # DELETE /instructions/1
-  # DELETE /instructions/1.json
   def destroy
     @instruction.destroy
-    respond_to do |format|
-      format.html { redirect_to instructions_url, notice: 'Instruction was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to @document, notice: 'Instruction was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_instruction
-      @instruction = Instruction.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_document_and_instruction
+    @document = Document.find(params[:document_id])
+    @instruction = Instruction.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def instruction_params
-      params.require(:instruction).permit(:content, :deadline, :document_id, :leader_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def instruction_params
+    params.require(:instruction).permit(:content, :deadline, :document_id, :leader_id)
+  end
 end
--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,10 @@
 module DocumentsHelper
+  def timeago(t)
+    delta = (t - Date.today).to_i
+    if delta >= 0
+      ["还有#{delta}天", 'warning']
+    else
+      ["已过#{-delta}天", 'danger']
+    end
+  end
 end
