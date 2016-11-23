8aad41fab06b1fc1d94febd0c236e269daf7bd32
Tom Mao <mr.fanyange@gmail.com>
2016-11-24 01:09:51 +0800

    automatially generate pinyin abbr of a leader

--- app/models/leader.rb
+++ app/models/leader.rb
@@ -14,6 +14,10 @@
 class Leader < ApplicationRecord
   has_many :instructions, dependent: :destroy
 
+  before_save { self.pinyin = PinYin.of_string(self.name).map { |value| value.to_s[0] }.join }
+
+  validates :name, presence: true
+
   def working
     Document.joins(:signed_leaders).order('locations.created_at DESC').group(:id).having('leaders.id = ?', id)
   end
